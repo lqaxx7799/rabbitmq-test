cb300b879a7feae43e296b3f6626a2e18c93d7e9
anhlq1 <anhlq1@novaon.vn>
2021-06-10 17:11:28 +0700

    retry connection

--- qanh-server/job/worker.js
+++ qanh-server/job/worker.js
@@ -1,5 +1,4 @@
 const amqp = require('amqplib');
-const { resolve } = require('bluebird');
 const config = require('../config');
 
 const assertQueueOptions = { durable: true };
@@ -7,25 +6,35 @@ const consumeQueueOptions = { noAck: false };
 const { uri, workQueue } = config;
 
 async function listenToQueue() {
-  const connection = await amqp.connect(uri);
-  const channel = await connection.createChannel();
-
-  console.log('Worker is running! Waiting for new messages...');
-  
-  const ackMsg = async (msg) => {
-    console.log('Received message');
-    await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log(msg.content.toString())
-        resolve();
-      }, 2000);
+  try {
+    const connection = await amqp.connect(uri);
+    connection.on('close', () => {
+      console.log('[Worker] Connection closed. Reconnecting in 10 seconds...');
+      return setTimeout(listenToQueue, 10 * 1000);
     });
-    return channel.ack(msg);
-  }
 
-  return channel.assertQueue(workQueue, assertQueueOptions)
-    .then(() => channel.prefetch(1))
-    .then(() => channel.consume(workQueue, ackMsg, consumeQueueOptions));
+    const channel = await connection.createChannel();
+
+    console.log('Worker is running! Waiting for new messages...');
+    
+    const ackMsg = async (msg) => {
+      console.log('Received message');
+      await doHeavyTask(2000);
+      console.log(msg.content.toString())
+      return channel.ack(msg);
+    }
+
+    return channel.assertQueue(workQueue, assertQueueOptions)
+      .then(() => channel.prefetch(1))
+      .then(() => channel.consume(workQueue, ackMsg, consumeQueueOptions));
+  } catch (e) {
+    console.log('[Worker] Connection failed');
+    return setTimeout(listenToQueue, 10 * 1000);
+  }
+}
+
+function doHeavyTask(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 module.exports = listenToQueue;
